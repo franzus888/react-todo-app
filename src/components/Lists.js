4cb36bb4829674ea372cd10753ff6ed6d2072b9b
franzus85 <franz.straube@gmail.com>
2018-11-27 13:14:41 +0100

    added component TodoList

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Todolist from './TodoList'
 // Fontawesome
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSort, faTimes} from '@fortawesome/free-solid-svg-icons';
@@ -7,15 +8,13 @@ import './Lists.scss';
 
 class Lists extends Component {
     render() {
-        const {
+        let {
             todos,
             showTodoBtns,
             hideTodoBtns,
             checkTodoDone,
             hoverTodo,
-            deleteTodo
-        } = this.props;
-        const {
+            deleteTodo,
             dones,
             showDoneBtns,
             hideDoneBtns,
@@ -25,41 +24,14 @@ class Lists extends Component {
         } = this.props;
         return (
             <div className="App-lists">
-                <ul className="App-todo-list">
-                    {todos.map((todo, index) => {
-                        return (
-                            <li
-                                className="App-list-item"
-                                key={index}
-                                onMouseOver={() => showTodoBtns(index)}
-                                onMouseOut={() => hideTodoBtns(index)}>
-                                <input
-                                    className="App-checkbox"
-                                    onChange={(e) => checkTodoDone(e, index)}
-                                    type="checkbox"
-                                    checked={todo.done}/>
-                                <span
-                                    className="App-todo-move"
-                                    style={{
-                                    display: hoverTodo === index
-                                        ? ''
-                                        : 'none'
-                                }}><FontAwesomeIcon className="App-svg-middle" icon={faSort}/></span>
-                                <span className="App-item-todo">{todo.title}</span>
-                                <span
-                                    className="App-todo-delete"
-                                    style={{
-                                    display: hoverTodo === index
-                                        ? ''
-                                        : 'none'
-                                }}><FontAwesomeIcon
-                                    onClick={() => deleteTodo(index)}
-                                    className="App-svg-delete"
-                                    icon={faTimes}/></span>
-                            </li>
-                        )
-                    })}
-                </ul>
+                <Todolist
+                    todos={todos}
+                    showTodoBtns={showTodoBtns}
+                    hideTodoBtns={hideTodoBtns}
+                    checkTodoDone={checkTodoDone}
+                    hoverTodo={hoverTodo}
+                    deleteTodo={deleteTodo}
+                />
                 <ul className="App-done-list">
                     {dones.map((done, index) => {
                         return (
---
+++ src/components/TodoList.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+// Fontawesome
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSort, faTimes} from '@fortawesome/free-solid-svg-icons';
+// Styles
+import './TodoList.scss';
+
+class TodoList extends Component {
+    render() {
+        let {
+            todos,
+            showTodoBtns,
+            hideTodoBtns,
+            checkTodoDone,
+            hoverTodo,
+            deleteTodo,
+        } = this.props;
+        return (
+                <ul className="App-todo-list">
+                    {todos.map((todo, index) => {
+                        return (
+                            <li
+                                className="App-list-item"
+                                key={index}
+                                onMouseOver={() => showTodoBtns(index)}
+                                onMouseOut={() => hideTodoBtns(index)}>
+                                <input
+                                    className="App-checkbox"
+                                    onChange={(e) => checkTodoDone(e, index)}
+                                    type="checkbox"
+                                    checked={todo.done}/>
+                                <span
+                                    className="App-todo-move"
+                                    style={{
+                                    display: hoverTodo === index
+                                        ? ''
+                                        : 'none'
+                                }}><FontAwesomeIcon className="App-svg-middle" icon={faSort}/></span>
+                                <span className="App-item-todo">{todo.title}</span>
+                                <span
+                                    className="App-todo-delete"
+                                    style={{
+                                    display: hoverTodo === index
+                                        ? ''
+                                        : 'none'
+                                }}><FontAwesomeIcon
+                                    onClick={() => deleteTodo(index)}
+                                    className="App-svg-delete"
+                                    icon={faTimes}/></span>
+                            </li>
+                        )
+                    })}
+                </ul>
+        )
+    };
+};
+
+export default TodoList;

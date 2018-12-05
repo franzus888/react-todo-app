5963c389757d12ca68e3e91ab4e7ef244b2271fa
franzus85 <franz.straube@gmail.com>
2018-12-05 11:57:42 +0100

    refactoring to functional components

---
+++ build/precache-manifest.457dd116690afb77db9279a2060b6421.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae8a8aa491224731663c",
+    "url": "/static/js/main.ae8a8aa4.chunk.js"
+  },
+  {
+    "revision": "52b75ad1ca4e8e432373",
+    "url": "/static/js/1.52b75ad1.chunk.js"
+  },
+  {
+    "revision": "ae8a8aa491224731663c",
+    "url": "/static/css/main.69d6a219.chunk.css"
+  },
+  {
+    "revision": "d094d070af0a28d9e75c79a55f07f2b8",
+    "url": "/index.html"
+  }
+];
